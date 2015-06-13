e5d4efd77df8829aada822c64a5d3b724f7544b5
tgoldenberg <thomasagoldenberg@gmail.com>
2015-06-13 13:53:20 -0400

    style landing page with gradient hero panel and stacked animal cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
     if session[:user_id]
       @user = User.find(session[:user_id])
-      @decks = DefaultDecks.all
     end
+    @decks = DefaultDeck.all
   end
 end
