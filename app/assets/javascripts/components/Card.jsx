ebf44daff1e383e8c62e1cc290a398f0a8ed71e8
tgoldenberg <thomasagoldenberg@gmail.com>
2015-06-14 03:06:04 -0400

    add flipper for cards

--- app/assets/javascripts/components/Card.jsx
+++ app/assets/javascripts/components/Card.jsx
@@ -1,12 +1,28 @@
 var Card = React.createClass({
+  flipCard: function() {
+    console.log("FLIP");
+  },
   render: function() {
+    console.log(this.props.card);
     return (
       <div className="col-md-5 card-image-holder">
-        <div className="image-wrapper">
-          <h1>{ this.props.card.name }</h1>
-          <img src={ this.props.card.image_url} height="370" />
+
+        <div className="card-flipper">
+
+          <div className="image-wrapper front-side animated" onClick={this.flipCard} >
+            <h1 className="flip-card-front" >{ this.props.card.name }</h1>
+            <img src={ this.props.card.image_url} height="370" id="large-image"/>
+          </div>
+
+          <div className="image-wrapper flip-side animated" onClick={this.flipCard}>
+            <h1 className="flip-card-front">{ this.props.card.name }</h1>
+            <img src={ this.props.card.image_url} height="270" />
+            <p>{this.props.card.description}</p>
+          </div>
+
         </div>
       </div>
+
     );
   }
 });
