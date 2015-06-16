41c35efd0f095da05ba4a2819543789835533d40
tgoldenberg <thomasagoldenberg@gmail.com>
2015-06-16 12:00:13 -0400

    add UsersController tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -29,13 +29,25 @@ RSpec.describe UsersController, :type => :controller do
 
   end
 
+  context "DELETE destroy_session" do
+    it 'should logout the user' do
+      user = User.create(email: "example@gmail.com", password: "password")
+      session[:user_id] = user.id
+      delete :destroy_session
+      expect(session[:user_id]).to eq(nil)
+    end
+  end
+
   # context "GET login" do
-  # end
-  #
-  # context "DELETE logout" do
+  #   it 'should provide a form for logging in' do
+  #     expect(assigns(:user)).to be_a_new(User)
+  #   end
   # end
   #
   # context "GET signup" do
+  #   it 'should provide a form for users to sign up' do
+  #     expect(assigns(:user)).to be_a_new(User)
+  #   end
   # end
 
 end
